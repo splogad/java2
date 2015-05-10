40bd94281b42d091ce96ac75bcc065a631735a99
splogad <splogad@gmail.com>
2015-05-10 17:41:37 +0000

    ok

--- HelloWorld/src/it/splogad/java/servlet/HelloWorld.java
+++ HelloWorld/src/it/splogad/java/servlet/HelloWorld.java
@@ -1,8 +1,12 @@
 package it.splogad.java.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,7 +14,9 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloWorld
  */
-@WebServlet("/HelloWorld")
+@WebServlet(urlPatterns="/HelloWorld", initParams={
+			@WebInitParam(name="type", value="checking")
+			})
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -26,6 +32,26 @@ public class HelloWorld extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		// Set response content type
+	    response.setContentType("text/html");
+	    PrintWriter pw = response.getWriter();
+	    insertCookie(response);
+	    
+	        
+	    pw.println("<html> <head></head>");
+		pw.println("<body>");
+		pw.println("<h1>Hello World</h1>");
+		
+		pw.println(request.getParameter("type"));
+		pw.println("</body></html>");
+		//response.sendRedirect("http://www.google.it");
+	}
+	
+	protected void insertCookie(HttpServletResponse res){
+		//Create a Simple Cookie that it wil be shown on the webpage
+		Cookie cookie_name = new Cookie("NomeDev", "splogad");
+		res.addCookie(cookie_name);
 	}
 
 	/**
