d1d9accf08a734ef16cb104f9f87f1ae7348292f
splogad <splogad@gmail.com>
2015-05-10 20:22:54 +0000

    Dispatcher Example

---
+++ HelloWorld/src/it/splogad/java/servlet/login/LogoutServlet.java
@@ -0,0 +1,44 @@
+package it.splogad.java.servlet.login;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+ 
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/LogoutServlet")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+        
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+        for(Cookie cookie : cookies){
+            if(cookie.getName().equals("JSESSIONID")){
+                System.out.println("JSESSIONID="+cookie.getValue());
+                break;
+            }
+        }
+        }
+        //invalidate the session if exists
+        HttpSession session = request.getSession(false);
+        System.out.println("User="+session.getAttribute("user"));
+        if(session != null){
+            session.invalidate();
+        }
+        RequestDispatcher rd = getServletContext().getRequestDispatcher("/Login.html");
+        PrintWriter out= response.getWriter();
+        out.println("<font color=green>Your session has been closed. See you soon!</font>");
+        rd.include(request, response);        
+    }
+ 
+}
